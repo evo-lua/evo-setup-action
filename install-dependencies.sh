4e45f37ad887555e82c057bd20ee7068ddc8b458
RDW <duckwhale@mail.ru>
2023-08-03 04:55:12 +0200

    Repo: Move the bash scripts inside the action file
    The original approach doesn't work; the scripts are not downloaded alongside the action itself and so the runtime can't be installed.
    
    This is pretty annoying and not how I thought it would work, but alas.

--- install-dependencies.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-PLATFORM=$(uname)
-
-if [ "$PLATFORM" = "Linux" ]; then
-	echo "Installing dependencies for Linux"
-	sudo apt-get update
-	sudo apt-get install --yes libgtk-3-0 libwebkit2gtk-4.0-37
-fi
